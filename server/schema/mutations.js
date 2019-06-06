6618e04c7244460a194199b9dd38f750b234284a
Fernando Brandao <fernando.brandao@adarga.ai>
2019-06-06 08:47:17 +0100

    first commit

---
+++ server/schema/mutations.js
@@ -0,0 +1,23 @@
+const graphql = require('graphql');
+const { GraphQLObjectType,
+        GraphQLString
+    } = graphql;
+const UserType = require('./types/user_types');
+
+const mutation = new GraphQLObjectType({
+    name: 'Mutation',
+    fields: {
+        signup: {
+            type: UserType,
+            args: {
+                email: { type: GraphQLString },
+                password: { type: GraphQLString }
+            },
+            resolve(parentValue, args, request) {
+                
+            }
+        }
+    }
+});
+
+module.exports = mutation;
